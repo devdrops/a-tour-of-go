0c5a2c3e3ddb406e62c8264b21b3f30475cda9b3
Davi Marcondes Moreira <davi.marcondes.moreira@gmail.com>
2019-03-26 15:48:30 -0300

    Module 6 WIP
    Current: https://tour.golang.org/concurrency/6

---
+++ module_6/default-selection.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	tick := time.Tick(100 * time.Millisecond)
+	boom := time.After(500 * time.Millisecond)
+
+	for {
+		select {
+		case:
+		}
+	}
+}
