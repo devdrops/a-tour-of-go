265ae4a7156a8842b82a3102a4bdee87717590fb
Davi Marcondes Moreira <davi.marcondes.moreira@gmail.com>
2019-04-12 15:06:31 -0300

    Module 6 is down
    This is the end, and it was a real fun!

--- module_6/exercise-equivalent-binary-trees.go
+++ module_6/exercise-equivalent-binary-trees.go
@@ -1,26 +1,54 @@
 package main
 
 import (
+	"fmt"
 	// Don't forget to `go get golang.org/x/tour/tree`
 	"golang.org/x/tour/tree"
 )
 
-func doTheWalk(t *tree.Tree, ch chan int) {
-	if t == nil {
+func doTheWalk(tree *tree.Tree, channel chan int) {
+	if tree == nil {
 		return
 	}
-	doTheWalk(t.Left, ch)
-	ch <- t.Value
-	doTheWalk(t.Right, ch)
+	doTheWalk(tree.Left, channel)
+	channel <- tree.Value
+	doTheWalk(tree.Right, channel)
 }
 
-func Walk(t *tree.Tree, ch chan int) {
-	doTheWalk(t, ch)
-	close(ch)
+func Walk(tree *tree.Tree, channel chan int) {
+	doTheWalk(tree, channel)
+	close(channel)
 }
 
-func Same(t1, t2 *tree.Tree) bool {
+func Same(firstTree, secondTree *tree.Tree) bool {
+	firstChannel, secondChannel := make(chan int), make(chan int)
+
+	go Walk(firstTree, firstChannel)
+	go Walk(secondTree, secondChannel)
+
+	for {
+		firstValue, firstOk := <-firstChannel
+		secondValue, secondOk := <-secondChannel
+		if !firstOk || !secondOk {
+			return firstOk == secondOk
+		}
+		if firstValue != secondValue {
+			return false
+		}
+	}
 }
 
 func main() {
+	fmt.Print("tree.New(1) == tree.New(1): ")
+	if Same(tree.New(1), tree.New(1)) {
+		fmt.Println("DEU BOM!")
+	} else {
+		fmt.Println("DEU RUIM!")
+	}
+	fmt.Print("tree.New(1) != tree.New(2): ")
+	if !Same(tree.New(1), tree.New(2)) {
+		fmt.Println("DEU BOM!")
+	} else {
+		fmt.Println("DEU RUIM!")
+	}
 }
